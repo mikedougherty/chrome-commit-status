3cb02dc69765fbae75e15f8dc0663fa269fb4eba
Mike Dougherty <mdougherty@tagged.com>
2012-12-26 15:35:36 -0800

    Remove samples

--- samples/simple-twitter/javascripts/options/options.js
+++
@@ -1,5 +0,0 @@
-document.onreadystatechange = function () {
-	if (document.readyState == "complete") {
-
-	}
-};
--- samples/simple-twitter/javascripts/popup/popup.js
+++
@@ -1,8 +0,0 @@
-document.onreadystatechange = function () {
-	if (document.readyState == "complete") {
-		
-		//this is the js used in the popup, when the 
-		//browser_action popup is executed
-		
-	}
-};
