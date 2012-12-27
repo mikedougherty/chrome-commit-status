cc5017fe84eb70144bb01273ff7f1219dcbf55b4
Mike Dougherty <mdougherty@tagged.com>
2012-12-26 16:05:51 -0800

    Removing some example code

--- javascripts/background.js
+++ javascripts/background.js
@@ -1,9 +1,4 @@
 document.onreadystatechange = function () {
-	if (document.readyState == "complete") {
-		
-		//once the background.html has been loaded
-		//insert your overall state of the extension here:
-		
-		
-	}
+    if (document.readyState == "complete") {
+    }
 };
