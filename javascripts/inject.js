21c6d249cc1be97171a72b2492c2ba959c0b7f90
Mike Dougherty <mdougherty@tagged.com>
2013-01-06 15:16:42 -0800

    Some more refactoring

--- javascripts/inject.js
+++ javascripts/inject.js
@@ -14,61 +14,70 @@ $(function() {
         return sha;
     };
 
-    var pageOwner = function() {
-        var match = /^\/(.+?)(?:\/.*)*$/.exec(URI(document.location).pathname());
-        if (match !== null) {
-            return match[1];
-        }
-        return null;
-    };
-
-    var pageProject = function() {
-        var match = /^\/(?:.+?)\/(.+?)(?:\/.*)*$/.exec(URI(document.location).pathname());
-        if (match !== null) {
-            return match[1];
-        }
-        return null;
-    };
-
-    var pageDomain = function() {
-        return document.location.host;
-    };
-
     var Project = function(options) {
         _.defaults(this, options, {
-            domain: pageDomain(),
-            owner: pageOwner(),
-            name: pageProject()
+            domain: this.pageDomain(),
+            owner: this.pageOwner(),
+            name: this.pageProject()
         });
     };
 
     _.extend(Project.prototype, {
         toString: function() {
             return _.string.sprintf("GitHub Project: https://%s/%s/%s", this.domain, this.owner, this.name);
+        },
+
+        pageDomain: function() {
+            return document.location.host;
+        },
+
+        pageProject: function() {
+            var match = /^\/(?:.+?)\/(.+?)(?:\/.*)*$/.exec(URI(document.location).pathname());
+            if (match !== null) {
+                return match[1];
+            }
+            return null;
+        },
+
+        pageOwner: function() {
+            var match = /^\/(.+?)(?:\/.*)*$/.exec(URI(document.location).pathname());
+            if (match !== null) {
+                return match[1];
+            }
+            return null;
         }
     });
 
-    var shas = [];
-    shas = shas.concat(_.map($('a.sha-block, a.sha, .commit-links a.gobutton'), function (el) {
-        var href = el.getAttribute('href');
-        match = /.*commit\/([0-9a-f]{40})/.exec(href);
-        if (match !== null) {
-            var sha = $(el).children('.sha');
-            if (sha.length === 0) {
-                sha = $(el, '.sha');
-            }
-            return processShaElement($(sha), match[1]);
-        }
-    }));
-    shas = shas.concat(_.map($('.sha-block .sha'), function(el) {
-        var text = $(el).text();
-        if (/[0-9a-f]{40}/.exec(text)) {
-            return processShaElement($(el), text);
-        }
-    }));
-    shas = _.unique(_.compact(shas));
+    var findAllShas = function (el) {
+        var shas = [];
+        var links = $(el).find('a.sha-block, a.sha, .commit-links a.gobutton');
+        var sha_blocks = $(el).find('.sha-block .sha');
 
-    _.each(shas, function (sha) {
+        shas = shas.concat(_.map(links, function (el) {
+            var href = el.getAttribute('href');
+            match = /.*commit\/([0-9a-f]{40})/.exec(href);
+            if (match !== null) {
+                var sha = $(el).children('.sha');
+                if (sha.length === 0) {
+                    sha = $(el, '.sha');
+                }
+                return processShaElement($(sha), match[1]);
+            }
+        }));
+
+        shas = shas.concat(_.map(sha_blocks, function (el) {
+            var text = $(el).text();
+            if (/[0-9a-f]{40}/.exec(text)) {
+                return processShaElement($(el), text);
+            }
+        }));
+
+        shas = _.unique(_.compact(shas));
+
+        return shas;
+    };
+
+    var updateShaStatus = function (sha) {
         var proj = new Project();
         var projData = localStorage[proj.toString()];
         if (!projData) {
@@ -96,5 +105,7 @@ $(function() {
             projData[sha] = latestStatus.state;
             setStatus(sha, latestStatus.state);
         });
-    });
+    };
+
+    _.each(findAllShas(document), updateShaStatus);
 });
